1548e0a43df2a3f76ecb293bc2ccf16d19cf8056
sagar bisht <168175036+sagarbisht1710@users.noreply.github.com>
2025-07-20 15:19:09 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import os
 import motor.motor_asyncio
 MONGO_URI = os.environ.get("MONGO_URl")
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-db = client.get_database()  # default DB from URI
+db = client.get_database("hrone-backend")  # default DB from URI
 app = FastAPI(title=settings.APP_NAME, version=settings.APP_VERSION)
 
 @app.get("/")
