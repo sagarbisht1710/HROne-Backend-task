120eb88fcac3db87556865c2be919a4e0f0c639a
sagar bisht <168175036+sagarbisht1710@users.noreply.github.com>
2025-07-20 12:52:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 export $(grep -v '^#' .env | xargs -d '\n')
-startCommand: uvicorn src.main:app --host 0.0.0.0 --port $PORT
+uvicorn src.main:app --host 0.0.0.0 --port $PORT
