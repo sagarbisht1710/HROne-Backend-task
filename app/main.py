7c2fc1602b5bf3d658bb3b2e6b37651dc8561d6d
sagar bisht <168175036+sagarbisht1710@users.noreply.github.com>
2025-07-20 15:17:02 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.routers import products, orders
-
+import os
+import motor.motor_asyncio
+MONGO_URI = os.environ.get("MONGO_URl")
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+db = client.get_database()  # default DB from URI
 app = FastAPI(title=settings.APP_NAME, version=settings.APP_VERSION)
 
 @app.get("/")
